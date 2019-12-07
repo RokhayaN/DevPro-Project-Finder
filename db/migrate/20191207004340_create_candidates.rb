4036ce3fda60e78b6229ca361cc785c3e8dbedc5
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-12-07 01:00:08 +0000

    Done.

---
+++ db/migrate/20191207004340_create_candidates.rb
@@ -0,0 +1,11 @@
+class CreateCandidates< ActiveRecord::Migration
+  def change
+  create_table  :candidates do |t|
+      t.string  :first_name 
+      t.string  :last_name 
+      t.string  :username
+      t.string  :email
+      t.string  :password 
+    end 
+  end
+end
