75d48757f3f1fff66b662c3ee398d83070a6e48d
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-12-09 04:31:30 +0000

    Done.

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -1,3 +1,5 @@
 class Engineer <ActiveRecord::Base
+  has_secure_password 
+  validates_presence_of :username,:email
   has_many :projects 
 end 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,3 @@
  class Project < ActiveRecord::Base
-   has_secure_password
-    validates_presence_of :username
-    validates_presence_of :email
    belongs_to :engineer 
  end 
