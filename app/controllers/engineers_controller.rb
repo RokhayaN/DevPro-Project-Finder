7d7ac483363e9117dc384630a05b6a2ea929d568
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-12-10 16:49:24 +0000

    Done.

---
+++ app/controllers/engineers_controller.rb
@@ -0,0 +1,68 @@
+ class EngineersController < ApplicationController
+
+
+	    get '/signup' do
+	        if !logged_in?
+	            erb :'/signup'
+	        else
+	            redirect '/projects'
+	        end
+	    end
+
+
+	    post '/signup' do
+	        if valid_signup? && !not_valid_username && !not_valid_email
+	            @engineer=Engineer.new(params) 
+	            @engineer.save
+	            session[:engineer_id]=@engineer.id
+	            redirect '/projects'  
+	        else  
+	            redirect 'failure'
+	        end
+	    end
+
+
+	    get '/sorry_signup' do
+	        erb :'/failure'
+	    end
+
+
+	    get '/login' do
+	        if !logged_in?
+	            erb :'/login'
+	        else
+	            redirect '/project' 
+	        end
+	    end
+
+
+	    post '/login' do
+	        @engineer = Engineer.find_by(username: params[:username])
+		    if @engineer && @engineer.authenticate(params[:password])
+				session[:engineer_id] = @engineer.id
+				redirect '/projects'
+			else
+				redirect '/failure'
+	        end
+	    end
+
+
+	    get '/failure' do
+	        erb :'failure'
+	    end
+
+
+	    get '/logout' do
+	        if logged_in?
+	            erb :'/logout'
+	        else
+	            redirect to "/"
+	        end 
+	    end
+
+
+	    post '/logout' do
+	        session.destroy
+	        redirect "/"
+	    end
+
