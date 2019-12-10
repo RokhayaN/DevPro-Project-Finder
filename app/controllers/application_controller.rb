cf04b8967d68a5b23b46dc183c194d5792284215
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-12-10 18:29:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,32 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "Hakuna Matata"
-  end 
+  end
   
+  get "/" do
+	    erb :homepage
+	  end
+
+	  helpers do
+
+	    def current_user
+	      @current_user ||= Engineer.find_by_id(session[:engineer_id]) if !! session[:engineer_id]
+	    end
+	    
+	    def logged_in?
+	      !!current_user
+	    end
+
+	    def valid_signup?
+	      params[:username] != "" && params[:email] != "" && params[:password] != "" && params[:first_name] != ""
+	    end
+
+	    def not_valid_username
+	      Engineer.all.find_by(username: params[:username])
+	    end
+
+	    def not_valid_email
+	      Engineer.all.find_by(email: params[:email])
+	    end
+	  end
+	end
